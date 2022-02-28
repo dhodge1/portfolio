09c7894167e24058d6c0bdd5684787ddfad88e5d
David Hodge <dhodge1@gmail.com>
2022-02-28 09:39:41 -0500

    Use storybook next addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,14 +7,10 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
-        },
-      },
-    },
+    "storybook-addon-next"
   ],
   "framework": "@storybook/react",
+  core: {
+    builder: 'webpack5'
+  }
 }
