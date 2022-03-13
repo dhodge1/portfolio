e3beec997337470877747d33875d31d02a22990b
David Hodge <dhodge1@gmail.com>
2022-03-13 16:05:13 -0400

    Adds optional collection title

--- components/Collection/Collection.js
+++ components/Collection/Collection.js
@@ -1,12 +1,17 @@
 import Card from '../Card/Card';
 
-const Collection = ({ cards }) => {
+const Collection = ({ cards, title = null }) => {
   return (
-    <div className="grid w-full gap-12 md:grid-cols-3 md:gap-6 lg:grid-cols-4 lg:gap-8">
-      {cards.map((card) => (
-        <Card {...card} key={card.title} />
-      ))}
-    </div>
+    <>
+      {title && (
+        <h1 className={`font-serif bold text-3xl md:text-4xl mb-8`}>{title}</h1>
+      )}
+      <div className="grid w-full gap-12 md:grid-cols-3 md:gap-6 lg:grid-cols-4 lg:gap-8">
+        {cards.map((card) => (
+          <Card {...card} key={card.title} />
+        ))}
+      </div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -35,6 +35,7 @@ export default function Home() {
   };
 
   const collection = {
+    title: 'Experiments',
     cards: [
       {
         img: {
